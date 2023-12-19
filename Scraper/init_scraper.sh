759aae1b94e7e2975ebe01a2cc31a866d56d89ca
Antoine Aubert <antoine.aubert@edu.esiee.fr>
2023-12-19 14:39:34 +0100

    Nouvelle architecture

--- Scraper/init_scraper.sh
+++
@@ -1,7 +0,0 @@
-echo "--------------------------"
-echo "<--   init_scraper.sh  -->"
-echo ""
-
-scrapy startproject twitch_crawler
-CD twitch_crawler && scrapy genspider twitch https://www.twitch.tv/
-scrapy shell "https://www.twitch.tv/"
