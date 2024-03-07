db41f0641aee7bc5c4e0ee985058213d67138916
=Benoit Marchadier <marchadier.benoit@gmail.com>
2024-03-07 18:25:01 +0100

    ajustement

--- dash/main.py
+++ dash/main.py
@@ -69,4 +69,4 @@ def pie(model_type):
 
 # pour run en localhost
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", debug=False)
+    app.run_server(host="0.0.0.0", port=8050, debug=False)
